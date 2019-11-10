1e9ebf6b09cf3e3d92bd8db3a4955c8ad83b970d
an-apluss <anuoluwapoakinseye@gmail.com>
2019-11-10 10:03:27 +0100

    protect the route from authorize user

--- server/src/routes/article.js
+++ server/src/routes/article.js
@@ -1,11 +1,13 @@
 import express from 'express';
 
 import articleController from '../controllers/articleController';
+import Auth from '../middleware/auth';
 
 const route = express.Router();
 
+const { checkToken } = Auth;
 const { fetchOneArticle } = articleController;
 
-route.get('/:articleId', fetchOneArticle);
+route.get('/:articleId', checkToken, fetchOneArticle);
 
 export default route;
