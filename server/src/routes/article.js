d0f7a65d422dda722a3661086907d3fd8198403e
an-apluss <anuoluwapoakinseye@gmail.com>
2019-11-10 12:06:06 +0100

    validate article ID

--- server/src/routes/article.js
+++ server/src/routes/article.js
@@ -2,12 +2,14 @@ import express from 'express';
 
 import articleController from '../controllers/articleController';
 import Auth from '../middleware/auth';
+import articleValidator from '../middleware/articleValidation';
 
 const route = express.Router();
 
 const { checkToken } = Auth;
 const { fetchOneArticle } = articleController;
+const { checkArticleId } = articleValidator;
 
-route.get('/:articleId', checkToken, fetchOneArticle);
+route.get('/:articleId', checkArticleId, checkToken, fetchOneArticle);
 
 export default route;
