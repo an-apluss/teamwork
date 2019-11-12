4467e093dbc58b7682b7cd1b70baa017f7fdbbc3
an-apluss <anuoluwapoakinseye@gmail.com>
2019-11-12 19:40:48 +0100

    protect the route from an unauthorized user

--- server/src/routes/feed.js
+++ server/src/routes/feed.js
@@ -1,11 +1,13 @@
 import express from 'express';
 
 import feedController from '../controllers/feedController';
+import Auth from '../middleware/auth';
 
 const route = express.Router();
 
 const { fetchPosts } = feedController;
+const { checkToken } = Auth;
 
-route.get('/', fetchPosts);
+route.get('/', checkToken, fetchPosts);
 
 export default route;
